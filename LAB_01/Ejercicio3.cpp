c308869610f91958351bcaec38319174a0e25a35
LeonDavisCoropuna <104030173+LeonDavisCoropuna@users.noreply.github.com>
2022-04-22 12:40:07 -0500

    Update Ejercicio3.cpp

--- LAB_01/Ejercicio3.cpp
+++ LAB_01/Ejercicio3.cpp
@@ -1,1 +1,17 @@
+#include <iostream>
+using namespace std;
 
+int main(){
+  
+    float x;
+    int y;
+    cout<<"Ingrese un numero: ";cin>>x;
+    y = int(x);
+    if(n-m < 0.5){
+        cout<<y;
+    }else{
+        cout<<y+1;
+    }
+    
+    return 0;
+}
