f458f7703f6f40dfd55e5008572620b0aa3b308a
LeonDavisCoropuna <104030173+LeonDavisCoropuna@users.noreply.github.com>
2022-04-22 12:42:09 -0500

    Update Ejercicio6.cpp

--- LAB_01/Ejercicio6.cpp
+++ LAB_01/Ejercicio6.cpp
@@ -1,1 +1,26 @@
+#include<iostream>
+#include<math.h>
+using namespace std;
 
+int main(){
+
+    int num;
+    while(true){
+        cout<<"Ingrese un numero entre 100 y 999: ";cin>>num; 
+        if(num >= 100 && num <= 999){
+            break;
+        } 
+    }
+
+    for(int i=9;i>=0;i--){
+        if(num >= pow(2,i)){
+            cout<<"1";
+            num = num - pow(2,i);
+        }
+        else{
+            cout<<"0";
+        }
+    }
+
+    return 0;
+}
