b57abe04fa0505b79a1d0ff931b1162fd6205ed1
LeonDavisCoropuna <104030173+LeonDavisCoropuna@users.noreply.github.com>
2022-04-22 12:47:21 -0500

    Update Ejercicio8.cpp

--- LAB_01/Ejercicio8.cpp
+++ LAB_01/Ejercicio8.cpp
@@ -1,1 +1,16 @@
+#include<iostream>
+#include <bits/stdc++.h>
 
+using namespace std;
+
+int main(){
+    int n,aux;
+    float media=0;
+    cout<<"Cantidad de numeros: ";cin>>n;
+    for(int i=0;i<n;i++){
+        cout<<"Numero: ";cin>>aux;
+        media += aux;
+    }
+    cout<<"Media: "<<media/n;
+    return 0;
+}
