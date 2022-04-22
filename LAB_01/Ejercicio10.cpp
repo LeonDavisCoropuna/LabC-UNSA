84a3fbc435f28fbdd1e8fed39ff07fed468f604b
LeonDavisCoropuna <104030173+LeonDavisCoropuna@users.noreply.github.com>
2022-04-22 12:36:35 -0500

    Update Ejercicio10.cpp

--- LAB_01/Ejercicio10.cpp
+++ LAB_01/Ejercicio10.cpp
@@ -1,1 +1,19 @@
+#include<iostream>
 
+using namespace std;
+
+int main(){
+    string password;
+    cout<<"Ingrese su contraseña: ";getline(cin,password);
+    while(true){
+        string aux;
+        cout<<"Vuelva a ingresar su contraseña: ";getline(cin,aux);
+        if(aux == password){
+            cout<<"Contraseña correcta";
+            break;
+        }
+        else{
+            cout<<"Contraseña incorrecta"<<endl;
+        }
+    }
+}
