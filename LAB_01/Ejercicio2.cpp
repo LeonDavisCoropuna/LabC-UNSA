65404e7d62e3f1d24e300a3270d11cbccea00f55
LeonDavisCoropuna <104030173+LeonDavisCoropuna@users.noreply.github.com>
2022-04-22 12:37:13 -0500

    Update Ejercicio2.cpp

--- LAB_01/Ejercicio2.cpp
+++ LAB_01/Ejercicio2.cpp
@@ -1,1 +1,26 @@
+#include<iostream>
 
+using namespace std;
+
+int main(){
+
+    string nombre,apellido1,apellido2,correo;
+
+    cout<<"Nombre: ";getline(cin,nombre);
+    cout<<"Apellido paterno: ";getline(cin,apellido1);
+    cout<<"Apellido materno: ";getline(cin,apellido2);
+
+    correo = nombre.substr(0,1);
+    correo = correo.append(apellido1);
+    correo = correo.append(apellido2.substr(0,1));
+
+    int tamanio = correo.size();
+    for(int i=0;i<tamanio;i++){
+        correo[i] = tolower(correo[i]);
+    }
+    correo = correo.append("@unsa.edu.pe");
+
+    cout<<correo;
+
+    return 0;
+}
