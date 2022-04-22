790a5453e2c605304f8ff15d4ba7e51007625569
LeonDavisCoropuna <104030173+LeonDavisCoropuna@users.noreply.github.com>
2022-04-22 12:43:39 -0500

    Update Ejercicio7.cpp

--- LAB_01/Ejercicio7.cpp
+++ LAB_01/Ejercicio7.cpp
@@ -1,1 +1,21 @@
+#include<iostream>
 
+using namespace std;
+
+int main(){
+
+    int a,b;
+    cout<<"Primer numero: ";cin>>a;
+    cout<<"Segundo numero: ";cin>>b;
+    if(a % b == 0){
+        cout<<b<<" es divisor de "<<a;
+    }
+    else if(b % a == 0){
+        cout<<a<<" es divisor de "<<b;
+    }
+    else{
+        cout<<"No son divisibles entre sí";
+    }
+
+    return 0;
+}
