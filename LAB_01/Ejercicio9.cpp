6956c91a4121612b3104810fc9616a42d3622fb4
LeonDavisCoropuna <104030173+LeonDavisCoropuna@users.noreply.github.com>
2022-04-22 12:53:54 -0500

    Update Ejercicio9.cpp

--- LAB_01/Ejercicio9.cpp
+++ LAB_01/Ejercicio9.cpp
@@ -1,1 +1,15 @@
+#include<iostream>
 
+using namespace std;
+
+int main(){
+    int aux,suma=0;
+    for(int i=0;i<10;i++){
+        cout<<"Escribe un numero: ";cin>>aux;
+        if(aux < 0){
+            suma += aux;
+        }
+    }
+    cout<<suma;
+    return 0;
+}
