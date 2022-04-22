46cd07c8c2503cc9bdb214f63ea326d0164ec6f2
LeonDavisCoropuna <104030173+LeonDavisCoropuna@users.noreply.github.com>
2022-04-22 13:27:00 -0500

    Update Ejercicio3.cpp

--- LAB_01/Ejercicio3.cpp
+++ LAB_01/Ejercicio3.cpp
@@ -1,17 +1,38 @@
-#include <iostream>
+#include<iostream>
 using namespace std;
 
 int main(){
-  
-    float x;
-    int y;
-    cout<<"Ingrese un numero: ";cin>>x;
-    y = int(x);
-    if(n-m < 0.5){
-        cout<<y;
-    }else{
-        cout<<y+1;
+
+    string tiempo, final = "AM";
+    int minutos,horas=0,dias=0;
+    cout<<"Ingrese la hora en formato HH:MM: ";getline(cin,tiempo);
+    cout<<"Tiempo en minutos: ";cin>>minutos;
+    int horat,minutot;
+    horat =stoi(tiempo.substr(0,2));  
+    minutot=stoi(tiempo.substr(3,2));
+    horat = horat*60;
+    minutos = minutos + horat + minutot;
+
+    while(true){
+        if(minutos >= 60){
+            horas += 1;
+            minutos -= 60;
+        }
+        if(horas > 24){
+            horas -= 24;
+        }
+        if(minutos < 60){
+            break;
+        }
     }
+    if(horas >= 12){
+        horas = horas-12;  
+        final = "PM";
+    }
+    if(horas == 00){
+        horas = 12;
+    }
+    cout<<"Hora de finalización: "<<horas<<":"<<minutos<<" "<<final;
     
     return 0;
 }
