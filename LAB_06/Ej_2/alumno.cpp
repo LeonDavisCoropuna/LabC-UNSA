986a7e3ec762625a0a13c0045a8b498d37b2f866
LeonDavisCoropuna <104030173+LeonDavisCoropuna@users.noreply.github.com>
2022-05-13 13:50:48 -0500

    Create alumno.cpp

---
+++ LAB_06/Ej_2/alumno.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+#include "alumno.h"
+
+int main(){
+
+    Alumno nuevo(20210691,"LEON FELIPE DAVIS COROPUNA",14.6,11.9,20);
+    nuevo.imprimir();
+    return 0;
+}
