6809cc581d4d21688a53fba3939ca2c054f34bc3
LeonDavisCoropuna <104030173+LeonDavisCoropuna@users.noreply.github.com>
2022-05-31 20:04:35 -0500

    Create Persona.h

---
+++ LAB_07/EJ_01/Persona.h
@@ -0,0 +1,13 @@
+#pragma once
+#include<iostream>
+
+using namespace std;
+
+class Persona{
+    protected:
+        int edad;
+        string nombre;
+    public:
+        Persona();
+        ~Persona();
+};
