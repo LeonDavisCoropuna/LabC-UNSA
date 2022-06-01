ac76e08b2da6eba340c4e92bdd39fbf2cfdcd37d
LeonDavisCoropuna <104030173+LeonDavisCoropuna@users.noreply.github.com>
2022-05-31 20:19:18 -0500

    Update operacionBase.h

--- LAB_07/Ej_05/operacionBase.h
+++ LAB_07/Ej_05/operacionBase.h
@@ -1,1 +1,14 @@
+#pragma once
+#include <iostream>
 
+using namespace std;
+
+class Base{
+    protected:
+        int operador;
+    public:
+        void setOperador(int);
+        int getOperador();
+        Base();
+        ~Base();
+};
