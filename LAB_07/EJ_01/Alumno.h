530ded6caa0c37a35d26499681ce208a4b64ec92
LeonDavisCoropuna <104030173+LeonDavisCoropuna@users.noreply.github.com>
2022-05-31 20:03:57 -0500

    Create Alumno.h

---
+++ LAB_07/EJ_01/Alumno.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#include "Persona.h"
+using namespace std;
+
+class Alumno : public Persona{
+    public:
+        void setAlumno();
+        void mostrar();
+        Alumno();
+        ~Alumno();
+};
