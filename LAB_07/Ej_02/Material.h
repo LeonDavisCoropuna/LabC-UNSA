aa24549c74e5f888eec98adda4b0cd1b6c41bb97
LeonDavisCoropuna <104030173+LeonDavisCoropuna@users.noreply.github.com>
2022-05-31 20:06:33 -0500

    Create Material.h

---
+++ LAB_07/Ej_02/Material.h
@@ -0,0 +1,11 @@
+#pragma once
+#include <bits/stdc++.h>
+#include "Color.h"
+
+class Material : public Color{
+    protected:
+        string material;
+    public:
+        Material();
+        ~Material();
+};
